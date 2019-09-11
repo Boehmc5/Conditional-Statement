ac404491a57aedac7fc5e4251ecb06888542c5f6
chris <chris@LAPTOP-T9OFFMSV>
2019-09-11 18:20:35 -0400

    <added some changes and comments>

--- Conditional Statement/Program.cs
+++ Conditional Statement/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/** 
+Christopher Boehm 
+Grade Calculator
+9/11/2019
+*/
+using System;
 
 namespace Conditional_Statement
 {
@@ -10,6 +11,45 @@ namespace Conditional_Statement
     {
         static void Main(string[] args)
         {
+            // asks for input value
+            Console.WriteLine("What grade do you expect to get in ISM 4300?");
+
+            try
+            {
+                //changes inputted value to an int
+                string input = Console.ReadLine();
+                int new_input = int.Parse(input);
+
+                //if statement to determine your grade
+                if(new_input >= 90)
+                {
+                    Console.WriteLine("You have recieved a A!");
+                }
+
+                else if(new_input >= 80)
+                {
+                    Console.WriteLine("You have recieved a B");
+                }
+
+                else if(new_input >= 70)
+                {
+                    Console.WriteLine("You have recieved a C");
+                }
+                else if(new_input >= 60)
+                {
+                    Console.WriteLine("You have recieved a D");
+                }
+                else
+                {
+                    Console.WriteLine("You have recieved a F");
+                }
+                Console.ReadLine();
+
+            }
+            catch
+            {
+                Console.WriteLine("You have an error");
+            }
         }
     }
 }
